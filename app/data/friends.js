5e1b45ac8cd1081054afe7ba66ee4128b5c5fca2
Kelly Nicholson <Kelly>
2017-03-11 10:00:19 -0500

    yesterday's work

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends.
+
+// ===============================================================================
+
+var friendsArray = [
+  {
+    name: "",
+    photo: "",
+    scores: [],
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
